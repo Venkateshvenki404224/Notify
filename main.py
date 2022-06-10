c0e0f574ac5504175238b52b925da86be7b17048
Venkateshvenki404224 <75518037+Venkateshvenki404224@users.noreply.github.com>
2022-06-10 00:17:42 -0300

    Add files via upload
    File Uploaded

---
+++ main.py
@@ -0,0 +1,57 @@
+from twilio.rest import Client
+import datetime
+# Getting the day
+day = datetime.date.today()
+Today = day.strftime("%A")
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = 'AC7b95626d9c0425256da8a60c0397b84f'
+auth_token = 'e6178ddb6f468184f6f0e40f46632ca8'
+client = Client(account_sid, auth_token)
+timetable = {
+    "Monday": {
+        '08:30-09:30': "ED",
+        '09:30-10:30': "Pysics",
+        '10:30-02:00': "Cs Lab",
+        '02:00-03:00': "Maths",
+        '03:00-04:00': "Cs",
+        '04:00-05:00': "Physics"
+    },
+    "Tuesday": {
+        '08:30-09:30': "ED",
+        '09:30-10:30': "Cs",
+        '10:30-11:30': "Maths",
+        '11:30-12:30': "Physics",
+        '01:00-04:00': "Maths Lab"
+    },
+    "Wednesday": {
+        '09:30-10:30': "Maths",
+        '10:30-11:30': "Cs",
+        '11:30-12:00': "Physics",
+        '01:00-04:00': "Maths Lab",
+        '04:00-05:00': "Cs"
+    },
+    "Thursday": {
+        '09:30-12:30': "Pysics Lab",
+        '01:00-02:00': "Cs",
+        '02:00-03:00': "Maths",
+        '03:00-04:00': "Physics"
+    },
+    "Friday": {
+        '09:30-10:30': "Cs",
+        '10:30-11:30': "Physics",
+        '11:30-12:30': "Maths",
+        '01:00-04:00': "Physics Lab",
+        '04:00-05:00': "Maths"
+    },
+    "Saturday": {
+        '9:30-12:30': "Cs Lab"
+    },
+}
+Day = timetable.get(Today)
+message = client.messages.create(
+                              body=f'Your Time Table for today is{Day}',
+                              from_='+16205828628',
+                              to='+918073088890'
+                          )
+print(message.status)
